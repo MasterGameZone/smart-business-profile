2dfbf3f19c6042f5185cf2289b2440d1e5f30ae3
MasterGameZone <mastersunil1451@gmail.com>
2026-07-02 00:47:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/smart-business-profile/",
+
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
     allowedHosts: true,
   },
-})
+});
