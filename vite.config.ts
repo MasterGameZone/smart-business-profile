616631379c5c145422b749e5960e8f7f26709482
MasterGameZone <mastersunil1451@gmail.com>
2026-07-02 13:45:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/smart-business-profile/",
-
+  
   server: {
     host: "0.0.0.0",
     port: 5000,
